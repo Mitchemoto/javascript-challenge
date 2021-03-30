3b78f8498a227f4f274ad60ac63f8432c45351ec
Mitchell <erich.mitchell87@gmail.com>
2021-03-29 23:02:37 -0600

    final works

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,85 +1,32 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-// individual way to input each row, do cell2 for next 
-// var row=d3.select('tbody').append('tr')
-
-// var cell=row.append('td')
-
-// cell.text('1/1/2010')
-
-for (var i=0;i<tableData.length;i++){
-	var row=d3.select('tbody').append('tr')
-	var cell_1=row.append('td')
-	cell_1.text(tableData[i]['datetime'])
-	var cell_2=row.append('td')
-	cell_2.text(tableData[i]['city'])
-	var cell_3=row.append('td')
-	cell_3.text(tableData[i]['state'])
-	var cell_4=row.append('td')
-	cell_4.text(tableData[i]['country'])
-	var cell_5=row.append('td')
-	cell_5.text(tableData[i]['shape'])
-	var cell_6=row.append('td')
-	cell_6.text(tableData[i]['durationMinutes'])
-	var cell_7=row.append('td')
-	cell_7.text(tableData[i]['comments'])
+// create a double forEach statement to append the data into a table
+function buildTable(data) {
+    d3.select("tbody").html("");
+    data.forEach((selection) => {
+        var tableRow = d3.select("tbody").append("tr");
+        Object.values(selection).forEach((value) => {
+            var tableData = tableRow.append("td");
+            tableData.text(value);
+        });
+    })
 }
 
-
-var input_date=d3.select('#datetime').property('value')
-var button=d3.select('#filter-btn')
-
-button.on('click', function(){console.log(d3.select('#datetime').property('value'))})
-
-console.log(tableData);
-
-// var filteredData=[]
-
-// for (var i=0; i<tableData.length; i++){
-// 	if (tableData[i]['datetime']=='1/5/2010')
-// }
-
-//below need to be completed. 
-
-var filteredData=tableData.filter((incident)=>function(incident){
-		//needs to return true or false
-		//so elements at the true index will be pushed to new array 
-		return incident['datetime']=='1/5/2010';
-
-})
-
-var filteredData=tableData.filter(function(incident){
-	if ('element'){
-		return true
-	} else {
-		return false
-	}
-})
-
-console.log(filteredData)
-
-// var variable_list=['datetime',
-// 				   'city',
-// 				   'state',
-// 				   'country',
-// 				   'shape',
-// 				   'durationMinutes',
-// 				   'comments']
-
-// var var_list=Object.keys(tableData[0]);
+// run the function over the dataset
+buildTable(tableData);
 
 
-// for (var i=0; i<tableData.length;i++){
-// 	 var row=d3.select('tbody').append('tr')
-// 	 for (var j=0; j<variable_list.length; i++){
-// 	 	var cell=row.append('td')
-// 	 	cell.text(tableData[i][variable_list[j]])
-// 	 }
-// }
+//create function for field search 
+function clickFilter() {
+    d3.event.preventDefault();
+    var date = d3.select("#datetime").property("value");
+    var filterDateTime = tableData;
+    if (date) {
+        filterDateTime = filterDateTime.filter((row) => row.datetime === date);
+    }
+    appendTable(filterDateTime);
+}
 
-
-
+d3.selectAll("#filter-btn").on("click", clickFilter);
 
